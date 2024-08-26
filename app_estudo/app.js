156cb0a745b52118a75f2211ee4efedf491d9e63
tiagoBarbano <tiago.barbano@gmail.com>
2024-08-26 19:16:53 -0300

    tete

--- app_estudo/app.js
+++ app_estudo/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 3100;
 
 app.get('/', (req, res) => {
   res.send('Hello World - Curso Porto NodeJS');
+});
+
+app.get('/health-check', (req, res) => {
+  res.send('UP');
 });
 
 app.listen(port, () => {
